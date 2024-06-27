f7b7a024e5293c292c74e056394f8a97ea1ac61a
theabdul97@gmail.com <theabdul97@gmail.com>
2024-06-27 23:34:25 +0530

    UpdateApi

--- src/main/java/com/eazybytes/accounts/service/IAccountsService.java
+++ src/main/java/com/eazybytes/accounts/service/IAccountsService.java
@@ -8,8 +8,25 @@ import com.eazybytes.accounts.dto.CustomerDto;
  */
 public interface IAccountsService {
 	
+	/**
+	 * @param customerDto
+	 */
 	void createAccount(CustomerDto customerDto);
 	
+	
+	/**
+	 * @param mobileNumber
+	 * @return
+	 */
 	CustomerDto fetchAccount(String mobileNumber);
+	
+	/**
+	 * @param customerDto
+	 * @return
+	 */
+	boolean updateAccount(CustomerDto customerDto);
+	
+	
+	
 	
 }
