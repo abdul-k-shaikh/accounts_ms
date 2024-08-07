d8b7611e9cd9f219131f9c951a637112d5504822
theabdul97@gmail.com <theabdul97@gmail.com>
2024-08-07 16:51:17 +0530

    InputValidationAtControllerLevel

--- src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -1,12 +1,14 @@
 package com.eazybytes.accounts.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,18 +19,21 @@ import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.dto.ResponseDto;
 import com.eazybytes.accounts.service.IAccountsService;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping(path = "/api", produces = { MediaType.APPLICATION_JSON_VALUE })
 @AllArgsConstructor
+@Validated
 public class AccountsController {
 
 //	@Autowired  here @AllArgsConstructor is helping , we comment this it wills how 500
 	private IAccountsService iAccountsService;
 
 	@PostMapping("/create")
-	public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+	public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
 		iAccountsService.createAccount(customerDto);
 		return ResponseEntity.status(HttpStatus.CREATED) // line 23 at header level status created
 				.body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
@@ -36,20 +41,40 @@ public class AccountsController {
 	}
 
 	@GetMapping("/fetch")
-	public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber) {
+	public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam 
+			@Pattern(regexp="(^$|[0-9]{10})", message="mobile number must 10 digits")
+			String mobileNumber) {
 		CustomerDto customerDto = iAccountsService.fetchAccount(mobileNumber);
 		return ResponseEntity.status(HttpStatus.OK).body(customerDto);
 
 	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<ResponseDto>updateAccountDetails(@Valid @RequestBody CustomerDto cusotomerDto){
+		boolean isUpdated = iAccountsService.updateAccount(cusotomerDto);
+		if(isUpdated) {
+			return ResponseEntity
+					.status(HttpStatus.OK)
+					.body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+		}else {
+			return ResponseEntity
+					.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
+
+		}	
+	}
 
 	@DeleteMapping("/delete")
-	public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam String mobileNumber) {
+	public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam 
+			@Pattern(regexp="(^$|[0-9]{10})", message="mobile number must 10 digits")
+			String mobileNumber) {
 		boolean isDeleted = iAccountsService.deleteAccount(mobileNumber);
 		if (isDeleted) {
 			return ResponseEntity.status(HttpStatus.OK)
 					.body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
 		} else {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+			return ResponseEntity
+					.status(HttpStatus.INTERNAL_SERVER_ERROR)
 					.body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
 
 		}
--- src/main/java/com/eazybytes/accounts/exception/GlobalExceptionHandler.java
+++ src/main/java/com/eazybytes/accounts/exception/GlobalExceptionHandler.java
@@ -1,18 +1,46 @@
 package com.eazybytes.accounts.exception;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.eazybytes.accounts.dto.ErrorResponseDto;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
 	
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		Map<String,String>validationErrors = new HashMap<>();
+		List<ObjectError>validationErrorList = ex.getBindingResult().getAllErrors();
+		
+		validationErrorList.forEach((error)->{
+			String fieldName = ((FieldError)error).getField();
+			String validationMsg = error.getDefaultMessage();
+			validationErrors.put(fieldName,validationMsg);
+		});
+		
+
+		return handleExceptionInternal(ex, null, headers, status, request);
+	}
+	
+	
+
 	/**
 	 * this checks all other runtime exceptions
 	 * @param exception
