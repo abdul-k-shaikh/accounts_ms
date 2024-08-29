fe8ae0d8cf418def49256dfbcc21e6bb74262964
theabdul97@gmail.com <theabdul97@gmail.com>
2024-08-29 19:04:36 +0530

    actuatorChanges

--- src/main/java/com/eazybytes/accounts/dto/AccountsContactInfoDto.java
+++ src/main/java/com/eazybytes/accounts/dto/AccountsContactInfoDto.java
@@ -5,7 +5,14 @@ import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message,Map<String, String>contactDetails,List<String> onCallSupport) {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "accounts")
+public class AccountsContactInfoDto {
+	String message;
+	Map<String, String> contactDetails;
+	List<String> onCallSupport;
 }
